962fc81270e4b4f009ac40d8f20952a6a7585ca3
Andrey Veres <spacecmetanka@gmail.com>
2024-10-21 21:57:14 +0300

    feat: add tests

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -17,7 +17,7 @@ describe('doStuffByTimeout', () => {
     jest.clearAllTimers();
   });
 
-  test('should set timeout with provided cb and timeout', () => {
+  test('should set timeout with provided callback  and timeout', () => {
     jest.spyOn(global, 'setTimeout');
     doStuffByTimeout(cb, ms);
 
@@ -25,7 +25,7 @@ describe('doStuffByTimeout', () => {
     expect(setTimeout).toHaveBeenCalledWith(cb, ms);
   });
 
-  test('should call cb only after timeout', () => {
+  test('should call callback only after timeout', () => {
     doStuffByTimeout(cb, ms);
     expect(cb).not.toHaveBeenCalled();
 
@@ -48,7 +48,7 @@ describe('doStuffByInterval', () => {
     jest.clearAllTimers();
   });
 
-  test('should set interval with provided cb and timeout', () => {
+  test('should set interval with provided callback and timeout', () => {
     jest.spyOn(global, 'setInterval');
 
     doStuffByInterval(cb, ms);
@@ -57,21 +57,28 @@ describe('doStuffByInterval', () => {
     expect(setInterval).toHaveBeenCalledWith(cb, ms);
   });
 
-  test('should call cb multiple times after multiple intervals', () => {
-    //
+  test('should call callback multiple times after multiple intervals', () => {
+    jest.spyOn(global, 'setInterval');
+    doStuffByInterval(cb, 1000);
+    expect(cb).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(1000);
+    jest.advanceTimersByTime(1000);
+
+    expect(cb).toHaveBeenCalledTimes(2);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    //
+    // Write your test here
   });
 
   test('should return null if file does not exist', async () => {
-    //
+    // Write your test here
   });
 
   test('should return file content if file exists', async () => {
-    //
+    // Write your test here
   });
 });
